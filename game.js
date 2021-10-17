794f691aedbd77b70dbbcb81f3efaa65dd26434f
PleaBanshee <llewellynant@gmail.com>
2021-10-17 08:11:22 +0200

    Added Progress Bar

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 // NB! === is a strict comparison (also checks for types). == just checks for values
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
+const progressBarFull = document.getElementById("progressBarFull");
 const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
@@ -54,7 +55,8 @@ let getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)*100}%`; // width must be percentage in this case
 
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
